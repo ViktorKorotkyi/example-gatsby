a432a5293c47de011a6fd43456775d426c33b27a
ViktorKorotkyi <viktor.korotkyi@onix-systems.com>
2024-06-19 00:32:01 +0300

    added zustand

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import { navigate } from "gatsby";
+import React from 'react';
+import { navigate } from 'gatsby';
 
 interface PrivateRouteProps {
   component: React.FC;
   location?: Location;
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ component: Component, location, ...rest }) => {
-  const isLoggedIn = typeof window !== "undefined" && localStorage.getItem("token");
+function PrivateRoute({ component: Component, location, ...rest }: PrivateRouteProps) {
+  const isLoggedIn = typeof window !== 'undefined' && localStorage.getItem('token');
 
-  if (!isLoggedIn && location && location.pathname !== `/`) {
-    navigate("/");
+  if (!isLoggedIn && location && location.pathname !== '/') {
+    navigate('/');
     return null;
   }
 
   return <Component {...rest} />;
+}
+
+PrivateRoute.defaultProps = {
+  location: undefined,
 };
 
 export default PrivateRoute;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 
+import useStore from '../store/test.ts';
 import { logoutTest } from '../utils/authMock.ts';
 import PrivateRoute from '../components/PrivateRoute.tsx';
 
 function DashboardPage() {
+  const count = useStore((state) => state.count);
+  const increaseCount = useStore((state) => state.increaseCount);
+  const decreaseCount = useStore((state) => state.decreaseCount);
+
   return (
     <main className="w-full min-h-screen flex items-center justify-center bg-[#c6def4]">
       <div className="w-[500px] flex items-center justify-between flex-col border p-6 pb-12 rounded-lg bg-[#fff]">
         <h1 className="text-[36px] z-10 font-bold mb-4">
           DASHBOARD
         </h1>
+        <div className="mt-4 mb-4 w-full flex flex-col items-center">
+          <span className="mb-2 text-base">State example</span>
+          <span className="mb-2 text-base">
+            {count}
+          </span>
+          <div className="w-full flex justify-between items-center gap-1">
+            <button
+              onClick={increaseCount}
+              className="w-full h-[40px] text-base rounded-md bg-slate-400 hover:opacity-80"
+              type="button"
+            >
+              Up
+            </button>
+            <button
+              onClick={decreaseCount}
+              className="w-full h-[40px] text-base rounded-md bg-slate-400 hover:opacity-80"
+              type="button"
+            >
+              Down
+            </button>
+          </div>
+        </div>
         <span>
           What is Lorem Ipsum?
           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
@@ -34,7 +61,7 @@ function DashboardPage() {
   );
 }
 
-export default function (props:React.FC<PageProps>) {
+export default function Dashboard(props: PageProps) {
   return <PrivateRoute component={DashboardPage} {...props} />;
 }
 
